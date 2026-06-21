18740614e97b39722373c9052c9188e41eb266d4
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-06-21 18:08:43 +0200

    [CLI, K/N] Deprecate `-Xworker-exception-handling` in 2.4.20 and remove in 2.5.0

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/SetupConfiguration.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/SetupConfiguration.kt
@@ -285,6 +285,7 @@ fun CompilerConfiguration.setupFromArguments(arguments: K2NativeCompilerArgument
             AllocationMode.CUSTOM
         }
     })
+    @Suppress("DEPRECATION_ERROR")
     when (arguments.workerExceptionHandling) {
         null -> {}
         "legacy" -> {
