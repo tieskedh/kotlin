053f3e090ec3607f9e968530c3b0b6ae957580fa
Ademi Abdykerimova <ademi.abdykerimova@jetbrains.com>
2026-06-23 12:37:37 +0600

    rewrite to fully functional tests

---
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/compilerArgumetns/JvmToolchainExternalAndroidTargetTest.kt
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.unitTests.compilerArgumetns
+
+import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.internal.project.ProjectInternal
+import org.gradle.api.tasks.compile.JavaCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerArgumentsProducer.CreateCompilerArgumentsContext.Companion.lenient
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.util.buildProject
+import org.jetbrains.kotlin.gradle.util.kotlin
+import org.jetbrains.kotlin.gradle.util.setAndroidSdkDirProperty
+import org.jetbrains.kotlin.gradle.utils.named
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class JvmToolchainExternalAndroidTargetTest {
+
+    @Test
+    fun `jvmToolchain configures compiler arguments for androidLibrary and androidHostTest`() {
+        val project = externalAndroidLibraryProject(
+            androidLibraryConfiguration = {
+                withHostTest {}
+            },
+            kotlinConfiguration = {
+                jvmToolchain(currentJvmToolchainVersion)
+            },
+        )
+        project.evaluate()
+
+        val expectedConfiguration = KotlinTaskConfiguration(
+            jvmTarget = currentJvmTarget,
+            toolchainVersion = currentJvmToolchainVersion,
+        )
+
+        assertEquals(expectedConfiguration, project.kotlinTaskConfiguration("compileAndroidMain"))
+        assertEquals(expectedConfiguration, project.kotlinTaskConfiguration("compileAndroidHostTest"))
+    }
+
+    @Test
+    fun `jvmToolchain configures Kotlin and Java tasks for androidLibrary withJava`() {
+        val project = externalAndroidLibraryProject(
+            kotlinConfiguration = {
+                jvmToolchain(currentJvmToolchainVersion)
+            },
+        )
+        project.evaluate()
+
+        assertEquals(
+            KotlinTaskConfiguration(
+                jvmTarget = currentJvmTarget,
+                toolchainVersion = currentJvmToolchainVersion,
+            ),
+            project.kotlinTaskConfiguration("compileAndroidMain"),
+        )
+        assertEquals(
+            JavaTaskConfiguration(toolchainVersion = currentJvmToolchainVersion),
+            project.javaTaskConfiguration("compileAndroidMainJavaWithJavac"),
+        )
+    }
+
+    @Test
+    fun `jvmToolchain and jvmTarget configure androidLibrary task independently`() {
+        val project = externalAndroidLibraryProject(
+            androidLibraryConfiguration = {
+                compilerOptions.jvmTarget.set(JvmTarget.JVM_11)
+            },
+            kotlinConfiguration = {
+                jvmToolchain(currentJvmToolchainVersion)
+            },
+        )
+        project.evaluate()
+
+        assertEquals(
+            KotlinTaskConfiguration(
+                jvmTarget = JvmTarget.JVM_11.target,
+                toolchainVersion = currentJvmToolchainVersion,
+            ),
+            project.kotlinTaskConfiguration("compileAndroidMain"),
+        )
+    }
+
+    private fun externalAndroidLibraryProject(
+        androidLibraryConfiguration: KotlinMultiplatformAndroidLibraryTarget.() -> Unit = {},
+        kotlinConfiguration: KotlinMultiplatformExtension.() -> Unit = {},
+    ): ProjectInternal = buildProject {
+        setAndroidSdkDirProperty(project)
+        plugins.apply("kotlin-multiplatform")
+        plugins.apply("com.android.kotlin.multiplatform.library")
+        kotlin {
+            targets.withType(KotlinMultiplatformAndroidLibraryTarget::class.java).configureEach { target ->
+                target.compileSdk = 34
+                target.namespace = "org.jetbrains.sample.multitarget"
+                target.withJava()
+                target.androidLibraryConfiguration()
+            }
+            kotlinConfiguration()
+        }
+    }
+
+    private fun Project.kotlinTaskConfiguration(taskName: String): KotlinTaskConfiguration {
+        val task = tasks.named<KotlinCompile>(taskName).get()
+        return KotlinTaskConfiguration(
+            jvmTarget = task.createCompilerArguments(lenient).jvmTarget.orEmpty(),
+            toolchainVersion = task.kotlinJavaToolchain.javaVersion.get().majorVersion.toInt(),
+        )
+    }
+
+    private fun Project.javaTaskConfiguration(taskName: String): JavaTaskConfiguration {
+        val task = tasks.named<JavaCompile>(taskName).get()
+        return JavaTaskConfiguration(
+            toolchainVersion = task.javaCompiler.get().metadata.languageVersion.asInt(),
+        )
+    }
+
+    private data class KotlinTaskConfiguration(
+        val jvmTarget: String,
+        val toolchainVersion: Int,
+    )
+
+    private data class JavaTaskConfiguration(
+        val toolchainVersion: Int,
+    )
+
+    private companion object {
+        val currentJvmToolchainVersion: Int = JavaVersion.current().majorVersion.toInt()
+        val currentJvmTarget: String = JvmTarget.fromTarget(JavaVersion.current().majorVersion).target
+    }
+}
