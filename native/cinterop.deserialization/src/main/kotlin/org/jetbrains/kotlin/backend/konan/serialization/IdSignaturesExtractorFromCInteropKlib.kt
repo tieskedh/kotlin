0e3cd4c85c8afda5ef46187042a692444c78d892
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-06-24 10:31:50 +0200

    fixup! C-interop signature extraction

--- native/cinterop.deserialization/src/main/kotlin/org/jetbrains/kotlin/backend/konan/serialization/IdSignaturesExtractorFromCInteropKlib.kt
+++ native/cinterop.deserialization/src/main/kotlin/org/jetbrains/kotlin/backend/konan/serialization/IdSignaturesExtractorFromCInteropKlib.kt
@@ -8,9 +8,15 @@ package org.jetbrains.kotlin.backend.konan.serialization
 import org.jetbrains.kotlin.K1Deprecation
 import org.jetbrains.kotlin.backend.common.IdSignaturesExtractor
 import org.jetbrains.kotlin.backend.common.IdSignaturesExtractor.ExtractedSignatures
+import org.jetbrains.kotlin.ir.declarations.impl.IrExternalPackageFragmentImpl
+import org.jetbrains.kotlin.ir.declarations.impl.IrFactoryImpl
+import org.jetbrains.kotlin.ir.symbols.impl.IrExternalPackageFragmentSymbolImpl
 import org.jetbrains.kotlin.ir.util.IdSignature
+import org.jetbrains.kotlin.ir.util.SymbolTable
 import org.jetbrains.kotlin.library.KotlinLibrary
 import org.jetbrains.kotlin.library.metadata.isCInteropLibrary
+import org.jetbrains.kotlin.library.packageFqName
+import org.jetbrains.kotlin.name.FqName
 
 /**
  * This is a lightweight tool that allows extracting [IdSignature]s from the given C-interop [KotlinLibrary].
@@ -20,6 +26,24 @@ class IdSignaturesExtractorFromCInteropKlib(library: KotlinLibrary) : IdSignatur
     init {
         check(library.isCInteropLibrary()) { "Not a C-interop library: $library" }
     }
+
+    private val packageFragment = IrExternalPackageFragmentImpl(
+        symbol = IrExternalPackageFragmentSymbolImpl(),
+        packageFqName = library.packageFqName?.let(::FqName) ?: error("C-interop library without the package name"),
+    )
+
+    private val symbolTable = SymbolTable(signaturer = null, IrFactoryImpl)
+    private val declarationTracker = CInteropKlibMetadata2IRTransformer.DeclarationTracker()
+
+    private val transformer = CInteropKlibMetadata2IRTransformer(
+        symbolTable = symbolTable,
+        symbols = CInteropKlibMetadata2IRTransformer.ExternalSymbols(symbolTable),
+        declarationTracker = declarationTracker,
+        getNestedKmClass = TODO(),
+        getOrCreateContainingPackageFragment = { packageFragment },
+        getReferencedDeclarationSymbol = TODO(),
+        irProviderForLazyAnnotations = TODO()
+    )
 
     override fun extractAllPublicSignatures(): ExtractedSignatures {
         TODO("Not yet implemented")
