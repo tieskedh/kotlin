7b3988b7ceae3770b3a9f7270d7e211492c95c71
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-06-25 16:39:12 +0200

    [JS] Add reproducers for KT-87009

--- compiler/testData/codegen/box/topLevelInitializtion/failInInitializer2.kt
+++ compiler/testData/codegen/box/topLevelInitializtion/failInInitializer2.kt
@@ -12,17 +12,30 @@ fun computeY(): String = "2"
 fun box() : String {
     try {
         x
-        return "FAIL 1"
+        return "FAIL 1.1"
     } catch(t: Error) {
         val cause = t.cause
-        if (cause !is IllegalStateException) return "FAIL 2"
-        if (cause.message != "1") return "FAIL 3"
+        if (cause !is IllegalStateException) return "FAIL 1.2: cause must be IllegalStateException, was ${cause?.let { it::class }}"
+        if (cause.message != "1") return "FAIL 1.3: message must be '1', was '${cause.message}'"
     }
     try {
         y
-        return "FAIL 4"
+        return "FAIL 2.1"
     } catch(t: Error) {
-        // On JVM < 20, t.cause is null, but on JVM >= 20, it's ExceptionInInitializerError.
+        if (JDK_MAJOR_VERSION < 20 || BACKEND_UNDER_TEST == "NATIVE") {
+            if (t.cause != null) return "FAIL 2.2.2: cause must be null, got ${t.cause}"
+        } else {
+            val cause = t.cause as? Error
+            if (cause == null) return "FAIL 2.3.2: cause must be Error, was ${t.cause}"
+            if (cause.cause !is IllegalStateException) return "FAIL 2.3.3: cause.cause must be IllegalStateException, was ${cause?.let { it::class }}"
+            if (cause.message != "1") return "FAIL 2.3.4: cause.message must be '1', was '${cause.message}'"
+        }
+        val expectedMessage = when (BACKEND_UNDER_TEST) {
+            "JVM_IR" -> "Could not initialize class LibKt"
+            "NATIVE" -> "There was an error during file or class initialization"
+            else -> "Could not initialize file"
+        }
+        if (t.message != expectedMessage) return "FAIL 2.4: message must be '$expectedMessage', was '${t.message}'"
     }
     return "OK"
 }
