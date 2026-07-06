484acab2ded3b151e471668bec4661c77a198b47
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-07-06 20:54:37 +0200

    [CLI] Mark `-Xsuppress-warning` as deprecated in 2.2.0 and update description
    According to 533d2f5ba6e6d2759d92d59b6004ee433214e262

--- compiler/cli/cli-base/gen/org/jetbrains/kotlin/cli/common/arguments/CommonCompilerArgumentsCopyGenerated.kt
+++ compiler/cli/cli-base/gen/org/jetbrains/kotlin/cli/common/arguments/CommonCompilerArgumentsCopyGenerated.kt
@@ -108,6 +108,7 @@ fun copyCommonCompilerArguments(from: CommonCompilerArguments, to: CommonCompile
     to.stdlibCompilation = from.stdlibCompilation
     to.suppressApiVersionGreaterThanLanguageVersionError = from.suppressApiVersionGreaterThanLanguageVersionError
     to.suppressVersionWarnings = from.suppressVersionWarnings
+    @Suppress("DEPRECATION")
     to.suppressedDiagnostics = from.suppressedDiagnostics.copyOf()
     to.unrestrictedBuilderInference = from.unrestrictedBuilderInference
     @Suppress("DEPRECATION")
