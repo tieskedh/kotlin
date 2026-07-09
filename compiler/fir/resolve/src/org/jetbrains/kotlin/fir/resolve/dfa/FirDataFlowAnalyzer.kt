060d3805029def07b966eff0048c3bd26fb721b9
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-07-09 20:21:04 +0300

    [DFA] Support `@EqualityBound`-based contracts for simple cases
     - Currently, only for `==` (not for explicit `equals` call).
     - Also, cases when `equals` is intersection override, or
    `@EqualityBound` must be inherited are not supported.
    
    ^KT-86127

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/FirDataFlowAnalyzer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/FirDataFlowAnalyzer.kt
@@ -758,6 +758,7 @@ abstract class FirDataFlowAnalyzer(
 
         context.processEqNotNullContracts()
         context.processEqContractsBasedOnPrimitiveEquals()
+        context.processEqContractBasedOnEqualityBound()
         context.processEqContractsForExhaustiveness()
     }
 
@@ -846,6 +847,14 @@ abstract class FirDataFlowAnalyzer(
 
         addEqualityImplications(leftOperandVariable, rightOperand)
         addEqualityImplications(rightOperandVariable, leftOperand)
+    }
+
+    private fun ProcessEqContext.processEqContractBasedOnEqualityBound() {
+        if (rightOperandVariable !is RealVariable) return
+        val boundForRhs = components.equalsOverrideContractCalculator.computeTypeForEqualityBoundBasedContract(leftOperand.resolvedType)
+        if (boundForRhs != null) {
+            flow.addImplication((expressionVariable eq isEq) implies (rightOperandVariable typeEq boundForRhs))
+        }
     }
 
     private inner class ProcessEqContext(
